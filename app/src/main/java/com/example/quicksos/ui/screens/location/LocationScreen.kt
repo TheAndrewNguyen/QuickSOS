46541871ffda1f6e3cf67f717820482b9af983ea
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2025-01-14 10:23:33 -0800

    added placeholders for location and settings

--- app/src/main/java/com/example/quicksos/ui/screens/location/LocationScreen.kt
+++ app/src/main/java/com/example/quicksos/ui/screens/location/LocationScreen.kt
@@ -1,8 +1,9 @@
 package com.example.quicksos.ui.screens.location
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun LocationScreen() {
-    TODO("Not yet implemented")
+    Text("Location Screen")
 }
--- app/src/main/java/com/example/quicksos/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/quicksos/ui/screens/settings/SettingsScreen.kt
@@ -1,8 +1,9 @@
 package com.example.quicksos.ui.screens.settings
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun SettingsScreen() {
-    TODO("Not yet implemented")
+    Text("Settings Screen")
 }
