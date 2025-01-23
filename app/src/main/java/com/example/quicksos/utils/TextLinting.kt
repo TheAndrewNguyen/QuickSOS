e8347e2d253a6fdfd8f65984a8cfe334c12585ce
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2025-01-22 16:26:27 -0800

    small code edit

--- app/src/main/java/com/example/quicksos/utils/TextLinting.kt
+++ app/src/main/java/com/example/quicksos/utils/TextLinting.kt
@@ -1,9 +1,6 @@
 package com.example.quicksos.utils
 
 fun validateTextLengthGreaterThanOne(value: String) : Boolean {
-    if(value.isEmpty()) {
-        return false
-    }
-
+    if(value.isEmpty()) return false;
     return true
 }
