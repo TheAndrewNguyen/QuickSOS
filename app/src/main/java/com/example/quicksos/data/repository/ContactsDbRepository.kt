95a6c83e4866b2900ff569061f504c2571e82407
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2025-01-28 15:07:10 -0800

    implemented more contact Repository code

--- app/src/main/java/com/example/quicksos/data/repository/ContactsDbRepository.kt
+++ app/src/main/java/com/example/quicksos/data/repository/ContactsDbRepository.kt
@@ -7,9 +7,16 @@ import javax.inject.Inject
 class ContactsDbRepository @Inject constructor(private val dao : ContactDao) {
     fun getAllContacts() : List<Contact> = dao.getAll()
 
-    fun insertContact(contact: Contact) {
-        dao.insert(contact)
-    }
+    //getting the exact contact form the search
+    fun getExactContact(firstName: String, lastName: String, phoneNumber: String) : List<Contact>
+    = dao.getExact(firstName, lastName, phoneNumber)
 
+    //insert a new contact
+    fun insertContact(contact: Contact) = dao.insert(contact)
+
+    //delete contact
+    fun deleteContact(contact: Contact) =  dao.delete(contact.uid)
+
+    //sort inputs
     fun sortByNameAsc() : List<Contact> = dao.sortByNameAsc()
 }
