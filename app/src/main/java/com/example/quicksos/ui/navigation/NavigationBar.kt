6429b11d0aa8f14dd172322f274cae2b216d710d
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2025-02-13 13:25:08 -0800

    changed names of Action Icon to Get Help

--- app/src/main/java/com/example/quicksos/ui/navigation/NavigationBar.kt
+++ app/src/main/java/com/example/quicksos/ui/navigation/NavigationBar.kt
@@ -1,6 +1,5 @@
 package com.example.quicksos.ui.navigation
 
-import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Home
@@ -18,20 +17,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.quicksos.ui.theme.QuickSOSTheme
 
-//items for NavBar
-val items = listOf("Action", "Location", "Emergency Contacts", "Settings")
+val navbarIconTitles = listOf("Get Help", "Location", "Emergency Contacts", "Settings")
 val selectedIcons = listOf(
     Icons.Filled.Home,
     Icons.Filled.LocationOn,
@@ -46,6 +42,7 @@ val unselectedIcons = listOf(
     Icons.Outlined.Settings
 )
 
+//NavBar composable
 @Composable
 fun NavBar(modifier: Modifier = Modifier, navController: NavController) {
 
@@ -70,7 +67,7 @@ fun NavBar(modifier: Modifier = Modifier, navController: NavController) {
     }
 
     NavigationBar(modifier = modifier) {
-        items.forEachIndexed { index, item ->
+        navbarIconTitles.forEachIndexed { index, item ->
             NavigationBarItem(
                 icon = {
                     Icon(
