50d97335a6b0829d2ea9496e80881fb23f79ace6
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2025-02-19 18:33:32 -0800

    set up viewModel uiState for Settings

--- app/src/main/java/com/example/quicksos/ui/screens/settings/viewModel/SettingsViewModel.kt
+++ app/src/main/java/com/example/quicksos/ui/screens/settings/viewModel/SettingsViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.quicksos.ui.screens.settings.viewModel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
 class SettingsViewModel : ViewModel() {
+    var uiState = mutableStateOf<UiState>(UiState.MainSettings)
+        private set
+}
 
+sealed class UiState {
+    data object MainSettings : UiState()
+    data object AppearanceSettings : UiState()
+    data object Text911Settings : UiState()
+    data object HelpSettings : UiState()
 }
